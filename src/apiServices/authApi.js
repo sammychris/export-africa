7d5fe9340914e39f3e627c84967d78c0b8544e87
Samuel Christopher <ebusameric@gmail.com>
2024-01-22 08:07:25 +0100

    create auth api services

--- src/apiServices/authApi.js
+++ src/apiServices/authApi.js
@@ -1,13 +1,25 @@
 import axios from 'axios';
 import { setAccessToken, getRefreshToken, setRefreshToken } from '@/utils/cookieUtils';
-const apiUrl = 'https://task-api.learninbit.app/api/v1/auth/token/';
+const apiUrl = 'http://localhost:3000/api/auth/';
 
 export const login = async (credentials) => {
     try {
-        const response = await axios.post(apiUrl, credentials);
-        setAccessToken(response.data);
-        setRefreshToken(response.data);
-        return response.data;
+        const { data } = await axios.post(`${apiUrl}login/`, credentials);
+        return data;
+        // setAccessToken(data?.token);
+        // setRefreshToken(response.data);
+        // return response.data;
+    } catch (error) {
+        console.error('Error fetching all data:', error);
+        throw error; // Rethrow the error for the calling component to handle
+    }
+};
+
+export const register = async (credentials) => {
+    try {
+        const { data } = await axios.post(`${apiUrl}register/`, credentials);
+        console.log({data})
+        return data;
     } catch (error) {
         console.error('Error fetching all data:', error);
         throw error; // Rethrow the error for the calling component to handle
@@ -17,7 +29,6 @@ export const login = async (credentials) => {
 export const refreshToken = async () => {
     try {
         const refresh = getRefreshToken(); // Retrieve from secure storage
-        console.log({refresh})
         const response = await axios.post(`${apiUrl}refresh/`, { refresh });
         setAccessToken(response.data);
     } catch (error) {
