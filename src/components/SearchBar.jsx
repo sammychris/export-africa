28dacec51a2d556ef5be960065e128dd4e363a16
Samuel Christopher <ebusameric@gmail.com>
2024-04-16 10:56:55 +0100

    add search bar component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,15 @@
+// components/SearchBar.js
+const SearchBar = () => {
+    return (
+      <div className="p-4">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-full border p-2 rounded"
+        />
+      </div>
+    );
+  };
+  
+  export default SearchBar;
+  
